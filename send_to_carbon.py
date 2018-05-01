009d631c8d92e25361d615fe1e0cb995b03b1576
Jared Winton <jwinton@unimelb.edu.au>
2018-05-01 16:51:01 +1000

    send to carbon module

---
+++ send_to_carbon.py
@@ -0,0 +1,40 @@
+import pickle
+import pickletools
+import struct
+import socket
+import logging
+try:
+    import ConfigParser as cfp
+except:
+    import configparser as cfp
+
+def sendtocarbon(carb_tuples):
+    # Load configuration file
+    with open("qos_test.ini") as f:
+        ini_config = f.read()
+    config = cfp.RawConfigParser(allow_no_value=True)
+    config.readfp(io.BytesIO(ini_config))
+
+    # Set Carbon server details
+    CARBON_SERVER = config.get('pingtest', 'CARBON_SERVER')
+    CARBON_PICKLE_PORT = config.get('pingtest', 'CARBON_PICKLE_PORT')
+
+    # Connect to Carbon server
+    sock = socket.socket()
+    try:
+        sock.connect((CARBON_SERVER, CARBON_PICKLE_PORT))
+    except socket.error:
+        logging.critical("Cannot connect to Carbon. Exception thrown: " + str(e))
+        raise SystemExit("Couldn't connect to %(server)s on port %(port)d, is carbon-cache.py running?" %
+                         {'server': CARBON_SERVER, 'port': CARBON_PICKLE_PORT})
+    except Exception as e:
+        logging.critical("Cannot connect to Carbon. Unexpected exception thrown: " + str(e))
+        raise
+
+    # Create package and send it to Carbon
+    package = pickle.dumps(carb_tuples, protocol=2)
+    carb_size = struct.pack('!L', len(package))
+    sock.sendall(carb_size)
+    sock.sendall(package)
+    pickletools.dis(package)
+    return
